8a28026c38bae415fbe19c09e26d07338d795e1e
DESKTOP-LGFKOFD\PANKAJ <pankaj.agade@htstechsolutions.com>
2023-03-17 11:26:37 +0530

    add update common data

--- ROUTE/commonData.js
+++ ROUTE/commonData.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var multer = require("multer");
 const { createClient } = require("@supabase/supabase-js");
 const supabase = createClient(
   "https://vuozzbzdmjgsltgmxrfo.supabase.co",
@@ -7,6 +8,7 @@ const supabase = createClient(
 );
 const bodyParser = require("body-parser");
 router.use(bodyParser.json());
+const storage = multer.memoryStorage();
 
 router.get("/", function (req, res, next) {
   res.send("respond with a resource from commonData.js");
@@ -25,6 +27,23 @@ router.get("/getHelpData", async (req, res) => {
 router.get("/getTermsAndConditionsData", async (req, res) => {
   const data = await supabase.from("common-data").select("terms_and_conditions").eq("id", 1).maybeSingle();
   res.send(data);
+});
+
+router.post("/updateCommonData", async (req, res) => {
+  let postData = { ...req.body };
+  const data = await supabase.from("common-data").update(postData).select("*").eq("id", 1).maybeSingle();
+  res.send(data);
+});
+
+router.post("/uploadEditorImage", multer({ storage: storage }).single("photo"), async (req, res) => {
+  supabase.storage.from("editor-images").upload(req.body.file_name, req.file.buffer).then(imgRes=> {
+    if(imgRes.data) {
+      // console.log("==> ", supabase.storage.from("editor-images").getPublicUrl(imgRes.data.path).data.publicUrl);
+      res.send({default: supabase.storage.from("editor-images").getPublicUrl(imgRes.data.path).data.publicUrl})
+    } else {
+      res.send(imgRes?.error?.message)
+    }
+  })
 });
 
 module.exports = router;
